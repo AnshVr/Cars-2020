c72c2b0d49301f6a4f0f9adb89310491ef59db83
AnshVr <85823104+AnshVr@users.noreply.github.com>
2021-06-13 13:48:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function spanelem() {
+  window.alert("Though there is an assortment of brand-new and fully redesigned passenger cars hitting the road for 2020 model year, their numbers are dwindling as buyers are instead choosing sport-utility vehicles as their rides of choice. Stalwart sedans like the Chevrolet Impala and Ford Taurus are being discontinued, with the number of small cars in particular thinning rapidly. While there were 26 subcompact models on the market as recently as 2016, they’re down to just 17 for 2019, with the Ford Fiesta and Chevrolet Sonic reportedly bowing out at the end of the current model year.Here’s a look at some of the most prominent introductions reaching dealers’ showrooms for 2020 for those who still aren’t averse to driving a sedan or coupe. We’ll catch up with the hottest new SUVs for the rest of you in a separate post.");
+  document.getElementById("audio").play();
+}
+
+function divelem() {
+  window.alert("Though there is an assortment of brand-new and fully redesigned passenger cars hitting the road for 2020 model year, their numbers are dwindling as buyers are instead choosing sport-utility vehicles as their rides of choice. Stalwart sedans like the Chevrolet Impala and Ford Taurus are being discontinued, with the number of small cars in particular thinning rapidly. While there were 26 subcompact models on the market as recently as 2016, they’re down to just 17 for 2019, with the Ford Fiesta and Chevrolet Sonic reportedly bowing out at the end of the current model year. Here’s a look at some of the most prominent introductions reaching dealers’ showrooms for 2020 for those who still aren’t averse to driving a sedan or coupe. We’ll catch up with the hottest new SUVs for the rest of you in a separate post..");
+  document.getElementById("audio").play();
+}
